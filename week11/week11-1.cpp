98ce539829d8c9c8fc7073c1229a1a9b37b35ae3
蕭泳嘉 <91250519+ritahsiao@users.noreply.github.com>
2022-05-06 11:53:57 +0800

    Update week11-1.cpp

--- week11/week11-1.cpp
+++ week11/week11-1.cpp
@@ -1,1 +1,21 @@
+///week11-1.cpp
+#include <iostream>///新///舊<stdio.h>
+#include <vector>
+char name[100][30];
+int grade[100];
+int main()
+{
+	int n;
+	///scanf("%d",&n);
+	std::cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		std::cin>>name[i]>>grade[i];///你的資料往右流到變數裡面
+	}
 
+	for(int i=0;i<n;i++)
+	{
+		std::cout<<name[i]<<std::endl;///end line表示換行
+		            ///送到左邊變數
+	}
+}
